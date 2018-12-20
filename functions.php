aa30e0c4b34a5970ec3fbc05c79e2eefdb01461d
Alexander Dryapko <adryapko@horoscope-inc.com>
2018-12-20 11:42:13 +0200

    Adding woocommerce support + woocommerce templates

--- functions.php
+++ functions.php
@@ -48,3 +48,9 @@ $acf_opt_args = array(
 	'icon_url'   => 'dashicons-hammer',
 );
 acf_add_options_page( $acf_opt_args );
+
+// WooCommerce support
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
